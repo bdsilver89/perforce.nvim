78de8b359df9a715a52d36dab0fbbff27ebe236b
Brian Silver <github@briansilver.net>
2023-07-20 23:40:36 -0400

    Fix bugs in attach file path and in missing word diff constant

--- lua/perforce/attach.lua
+++ lua/perforce/attach.lua
@@ -16,162 +16,162 @@ local vimgrep_running = false
 local M = {}
 
 local function on_lines(_, bufnr, _, first, last_orig, last_new, byte_count)
-	if first == last_orig and last_orig == last_new and byte_count == 0 then
-		-- on_lines might be called twice for undo events
-		-- this ignores the second call which indicates no change
-		return
-	end
-	return manager.on_lines(bufnr, first, last_orig, last_new)
+  if first == last_orig and last_orig == last_new and byte_count == 0 then
+    -- on_lines might be called twice for undo events
+    -- this ignores the second call which indicates no change
+    return
+  end
+  return manager.on_lines(bufnr, first, last_orig, last_new)
 end
 
 local function on_reload(_, bufnr)
-	manager.update(bufnr)
+  manager.update(bufnr)
 end
 
 local function on_detach(_, bufnr)
-	M.detach(bufnr, true)
+  M.detach(bufnr, true)
 end
 
 local done_setup = false
 function M._setup()
-	if done_setup then
-		return
-	end
-	done_setup = true
+  if done_setup then
+    return
+  end
+  done_setup = true
 
-	manager.setup()
+  manager.setup()
 
-	hl.setup()
-	vim.api.nvim_create_autocmd("ColorScheme", {
-		group = "perforce",
-		callback = hl.setup,
-	})
+  hl.setup()
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    group = "perforce",
+    callback = hl.setup,
+  })
 
-	vim.api.nvim_create_autocmd("OptionSet", {
-		group = "perforce",
-		pattern = "fileformat",
-		callback = function()
-			require("perforce.actions").refresh()
-		end,
-	})
+  vim.api.nvim_create_autocmd("OptionSet", {
+    group = "perforce",
+    pattern = "fileformat",
+    callback = function()
+      require("perforce.actions").refresh()
+    end,
+  })
 
-	vim.api.nvim_create_autocmd("QuickFixCmdPre", {
-		group = "perforce",
-		pattern = "*vimgrep*",
-		callback = function()
-			vimgrep_running = true
-		end,
-	})
+  vim.api.nvim_create_autocmd("QuickFixCmdPre", {
+    group = "perforce",
+    pattern = "*vimgrep*",
+    callback = function()
+      vimgrep_running = true
+    end,
+  })
 
-	vim.api.nvim_create_autocmd("QuickFixCmdPost", {
-		group = "perforce",
-		pattern = "*vimgrep*",
-		callback = function()
-			vimgrep_running = false
-		end,
-	})
+  vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+    group = "perforce",
+    pattern = "*vimgrep*",
+    callback = function()
+      vimgrep_running = false
+    end,
+  })
 
-	vim.api.nvim_create_autocmd("VimLeavePre", {
-		group = "perforce",
-		callback = M.detach_all,
-	})
+  vim.api.nvim_create_autocmd("VimLeavePre", {
+    group = "perforce",
+    callback = M.detach_all,
+  })
 end
 
 function M.attach(bufnr, ctx, trigger)
-	bufnr = bufnr or vim.api.nvim_get_current_buf()
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
 
-	M._setup()
+  M._setup()
 
-	if vimgrep_running then
-		return
-	end
+  if vimgrep_running then
+    return
+  end
 
-	if not vim.api.nvim_buf_is_loaded(bufnr) then
-		utils.warn("Skip attach to unloaded buffer")
-		return
-	end
+  if not vim.api.nvim_buf_is_loaded(bufnr) then
+    utils.warn("Skip attach to unloaded buffer")
+    return
+  end
 
-	local encoding = vim.bo[bufnr].fileencoding
-	if encoding == "" then
-		encoding = "utf-8"
-	end
+  local encoding = vim.bo[bufnr].fileencoding
+  if encoding == "" then
+    encoding = "utf-8"
+  end
 
-	-- local file --- @type string
-	-- if ctx then
-	-- else
-	-- end
+  -- local file --- @type string
+  -- if ctx then
+  -- else
+  -- end
 
-	local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":t")
-	local p4_file = p4.File.new(path)
-	if not p4_file then
-		utils.warn("Could not create")
-		return
-	end
+  local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":p")
+  local p4_file = p4.File.new(path)
+  if not p4_file then
+    utils.warn("Could not create")
+    return
+  end
 
-	if not p4_file.exists_in_depot then
-		-- NOTE: do not warn here!
-		-- until I find a better way to detect p4 workspaces and validate that this file is in a workspace,
-		-- this is the only mechanism that prevents non-perforce workspace files from being loaded
-		-- adding a warning here would bombard non-p4 workspaces with excessive warning messages
-		return
-	end
+  if not p4_file.exists_in_depot then
+    -- NOTE: do not warn here!
+    -- until I find a better way to detect p4 workspaces and validate that this file is in a workspace,
+    -- this is the only mechanism that prevents non-perforce workspace files from being loaded
+    -- adding a warning here would bombard non-p4 workspaces with excessive warning messages
+    return
+  end
 
-	-- TODO: update a status
-	-- Status:update(bufnr, {})
-	-- TODO: check the paths
+  -- TODO: update a status
+  -- Status:update(bufnr, {})
+  -- TODO: check the paths
 
-	cache[bufnr] = CacheEntry.new({
-		p4_file = p4_file,
-	})
+  cache[bufnr] = CacheEntry.new({
+    p4_file = p4_file,
+  })
 
-	if not vim.api.nvim_buf_is_loaded(bufnr) then
-		utils.warn("Skip attach to unloaded buffer")
-		return
-	end
+  if not vim.api.nvim_buf_is_loaded(bufnr) then
+    utils.warn("Skip attach to unloaded buffer")
+    return
+  end
 
-	vim.api.nvim_buf_attach(bufnr, false, {
-		on_lines = on_lines,
-		on_reload = on_reload,
-		on_detach = on_detach,
-	})
+  vim.api.nvim_buf_attach(bufnr, false, {
+    on_lines = on_lines,
+    on_reload = on_reload,
+    on_detach = on_detach,
+  })
 
-	if config.open_on_change then
-		vim.api.nvim_create_autocmd("FileChangedRO", {
-			group = "perforce",
-			callback = function()
-				require("perforce.actions").edit({ bufnr = bufnr })
-			end,
-			buffer = bufnr,
-		})
-	end
+  if config.open_on_change then
+    vim.api.nvim_create_autocmd("FileChangedRO", {
+      group = "perforce",
+      callback = function()
+        require("perforce.actions").edit({ bufnr = bufnr })
+      end,
+      buffer = bufnr,
+    })
+  end
 
-	vim.api.nvim_buf_attach(bufnr, false, {
-		on_lines = on_lines,
-		on_reload = on_reload,
-		on_detach = on_detach,
-	})
+  vim.api.nvim_buf_attach(bufnr, false, {
+    on_lines = on_lines,
+    on_reload = on_reload,
+    on_detach = on_detach,
+  })
 
-	manager.update(bufnr, cache[bufnr])
+  manager.update(bufnr, cache[bufnr])
 end
 
 function M.detach_all()
-	for k, _ in pairs(cache) do
-		M.detach(k)
-	end
+  for k, _ in pairs(cache) do
+    M.detach(k)
+  end
 end
 
 ---@param bufnr integer
 ---@param keep_signs? boolean
 function M.detach(bufnr, keep_signs)
-	bufnr = bufnr or vim.api.nvim_get_current_buf()
-	local bcache = cache[bufnr]
-	if not bcache then
-		return
-	end
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+  local bcache = cache[bufnr]
+  if not bcache then
+    return
+  end
 
-	manager.detach(bufnr, keep_signs)
-	Status:clear(bufnr)
-	pcache.destroy(bufnr)
+  manager.detach(bufnr, keep_signs)
+  Status:clear(bufnr)
+  pcache.destroy(bufnr)
 end
 
 return M
--- lua/perforce/diff/internal.lua
+++ lua/perforce/diff/internal.lua
@@ -8,15 +8,15 @@ local M = {}
 
 --- @type Perforce.Difffn
 local run_diff_xdl = function(fa, fb, algorithm, indent_heuristic, linematch)
-	local a = vim.tbl_isempty(fa) and "" or table.concat(fa, "\n") .. "\n"
-	local b = vim.tbl_isempty(fb) and "" or table.concat(fb, "\n") .. "\n"
+  local a = vim.tbl_isempty(fa) and "" or table.concat(fa, "\n") .. "\n"
+  local b = vim.tbl_isempty(fb) and "" or table.concat(fb, "\n") .. "\n"
 
-	return vim.diff(a, b, {
-		result_type = "indices",
-		algorithm = algorithm,
-		indent_heuristic = indent_heuristic,
-		linematch = linematch,
-	}) --[[@as Perforce.RawHunk[] ]]
+  return vim.diff(a, b, {
+    result_type = "indices",
+    algorithm = algorithm,
+    indent_heuristic = indent_heuristic,
+    linematch = linematch,
+  }) --[[@as Perforce.RawHunk[] ]]
 end
 
 ---@param fa string[]
@@ -26,50 +26,52 @@ end
 ---@param linematch? integer
 ---@return Perforce.Hunk[]
 function M.run_diff(fa, fb, diff_algo, indent_heuristic, linematch)
-	-- TODO: threading/async?
-	local results = run_diff_xdl(fa, fb, diff_algo, indent_heuristic, linematch)
+  -- TODO: threading/async?
+  local results = run_diff_xdl(fa, fb, diff_algo, indent_heuristic, linematch)
 
-	local hunks = {} ---@type Perforce.Hunk[]
-	for _, r in ipairs(results) do
-		local rs, rc, as, ac = r[1], r[2], r[3], r[4]
-		local hunk = create_hunk(rs, rc, as, ac)
-		if rc > 0 then
-			for i = rs, rs + rc - 1 do
-				hunk.removed.lines[#hunk.removed.lines + 1] = fa[i] or ""
-			end
-		end
-		if ac > 0 then
-			for i = as, as + ac - 1 do
-				hunk.added.lines[#hunk.added.lines + 1] = fb[i] or ""
-			end
-		end
-		hunks[#hunks + 1] = hunk
-	end
-	return hunks
+  local hunks = {} ---@type Perforce.Hunk[]
+  for _, r in ipairs(results) do
+    local rs, rc, as, ac = r[1], r[2], r[3], r[4]
+    local hunk = create_hunk(rs, rc, as, ac)
+    if rc > 0 then
+      for i = rs, rs + rc - 1 do
+        hunk.removed.lines[#hunk.removed.lines + 1] = fa[i] or ""
+      end
+    end
+    if ac > 0 then
+      for i = as, as + ac - 1 do
+        hunk.added.lines[#hunk.added.lines + 1] = fb[i] or ""
+      end
+    end
+    hunks[#hunks + 1] = hunk
+  end
+  return hunks
 end
+
+local gaps_between_regions = 5
 
 --- @param hunks Perforce.Hunk[]
 --- @return Perforce.Hunk[]
 local function denoise_hunks(hunks)
-	-- Denoise the hunks
-	local ret = { hunks[1] } --- @type Perforce.Hunk[]
-	for j = 2, #hunks do
-		local h, n = ret[#ret], hunks[j]
-		if not h or not n then
-			break
-		end
-		if n.added.start - h.added.start - h.added.count < gaps_between_regions then
-			h.added.count = n.added.start + n.added.count - h.added.start
-			h.removed.count = n.removed.start + n.removed.count - h.removed.start
+  -- Denoise the hunks
+  local ret = { hunks[1] } --- @type Perforce.Hunk[]
+  for j = 2, #hunks do
+    local h, n = ret[#ret], hunks[j]
+    if not h or not n then
+      break
+    end
+    if n.added.start - h.added.start - h.added.count < gaps_between_regions then
+      h.added.count = n.added.start + n.added.count - h.added.start
+      h.removed.count = n.removed.start + n.removed.count - h.removed.start
 
-			if h.added.count > 0 or h.removed.count > 0 then
-				h.type = "change"
-			end
-		else
-			ret[#ret + 1] = n
-		end
-	end
-	return ret
+      if h.added.count > 0 or h.removed.count > 0 then
+        h.type = "change"
+      end
+    else
+      ret[#ret + 1] = n
+    end
+  end
+  return ret
 end
 
 ---@param removed string[]
@@ -77,38 +79,38 @@ end
 ---@return Perforce.Region[] removed
 ---@return Perforce.Region[] added
 function M.run_word_diff(removed, added)
-	local adds = {} ---@type Perforce.Region[]
-	local rems = {} ---@type Perforce.Region[]
+  local adds = {} ---@type Perforce.Region[]
+  local rems = {} ---@type Perforce.Region[]
 
-	if #removed ~= #added then
-		return rems, adds
-	end
+  if #removed ~= #added then
+    return rems, adds
+  end
 
-	for i = 1, #removed do
-		local a, b = vim.split(removed[i], ""), vim.split(added[i], "")
+  for i = 1, #removed do
+    local a, b = vim.split(removed[i], ""), vim.split(added[i], "")
 
-		local hunks = {} ---@type Perforce.Hunk[]
-		for _, r in ipairs(run_diff_xdl(a, b)) do
-			local rs, rc, as, ac = r[1], r[2], r[3], r[4]
+    local hunks = {} ---@type Perforce.Hunk[]
+    for _, r in ipairs(run_diff_xdl(a, b)) do
+      local rs, rc, as, ac = r[1], r[2], r[3], r[4]
 
-			if rc == 0 then
-				rs = rs + 1
-			end
-			if ac == 0 then
-				as = as + 1
-			end
+      if rc == 0 then
+        rs = rs + 1
+      end
+      if ac == 0 then
+        as = as + 1
+      end
 
-			hunks[#hunks + 1] = create_hunk(rs, rc, as, ac)
-		end
+      hunks[#hunks + 1] = create_hunk(rs, rc, as, ac)
+    end
 
-		hunks = denoise_hunks(hunks)
+    hunks = denoise_hunks(hunks)
 
-		for _, h in ipairs(hunks) do
-			adds[#adds + 1] = { i, h.type, h.added.start, h.added.start + h.added.count }
-			rems[#adds + 1] = { i, h.type, h.removed.start, h.removed.start + h.removed.count }
-		end
-	end
-	return rems, adds
+    for _, h in ipairs(hunks) do
+      adds[#adds + 1] = { i, h.type, h.added.start, h.added.start + h.added.count }
+      rems[#adds + 1] = { i, h.type, h.removed.start, h.removed.start + h.removed.count }
+    end
+  end
+  return rems, adds
 end
 
 return M
